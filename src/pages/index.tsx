35e091732a244dd0115f48b723434a3febf3a6a9
Bryan Zapata <bmzc95@gmail.com>
2021-03-23 21:36:15 -0500

    Dinamic title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
+import Head  from "next/head";
 import styled from "../styles/home.module.scss";
 
 export default function Home() {
   return (
-    <h1 className={styled.title}>Hello world</h1>
+    <>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+      <h1 className={styled.title}>Hello world</h1>
+    </>
   )
 }
