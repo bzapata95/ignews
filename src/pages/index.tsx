2e8a53bddfe0dbcbcfda10af05fb08c8157ec0bd
Bryan Zapata <bmzc95@gmail.com>
2021-03-23 21:46:01 -0500

    Global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head  from "next/head";
-import styled from "../styles/home.module.scss";
 
 export default function Home() {
   return (
@@ -7,7 +6,7 @@ export default function Home() {
       <Head>
         <title>Inicio | ig.news</title>
       </Head>
-      <h1 className={styled.title}>Hello world</h1>
+      <h1>Hello world</h1>
     </>
   )
 }
