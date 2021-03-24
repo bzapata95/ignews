c2dc922fb2f342082d1135ca14e27a58a758b749
Bryan Zapata <bmzc95@gmail.com>
2021-03-23 23:07:26 -0500

    finish header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,10 @@
+import {memo} from 'react';
+
+import SignInButton from "../SignInButton";
+
 import styles from './styles.module.scss';
 
-export default function Header() {
+function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
@@ -10,8 +14,12 @@ export default function Header() {
           <a className={styles.active}>Home</a>
           <a>Posts</a>
         </nav>
+
+        <SignInButton />
       </div>
     </header>
   );
 };
 
+
+export default memo(Header);
