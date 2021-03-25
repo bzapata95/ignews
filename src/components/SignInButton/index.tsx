bdbd44a99c80079b245e3b6f79b684d8e6d8db61
Bryan Zapata <bmzc95@gmail.com>
2021-03-25 02:07:24 -0500

    added faunadb

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,13 +1,12 @@
-import {memo} from "react";
-import {FaGithub} from 'react-icons/fa';
-import {FiX} from 'react-icons/fi';
+import { memo } from "react";
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
 import { signIn, signOut, useSession } from 'next-auth/client';
 
 import  styles from "./styles.module.scss";
 
 function SignInButton() {
   const [session] = useSession();
-  console.log(session);
 
   return session ? (
     <button type="button" className={styles.signInButton} onClick={() => signOut()}>
