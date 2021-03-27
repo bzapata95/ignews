0cb2b06e5e2bc1edddcc44ab3e3ce2b184cccedf
Bryan Zapata <bmzc95@gmail.com>
2021-03-27 16:17:14 -0500

    validations of subscription

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,5 @@
 import { signIn, useSession } from 'next-auth/client';
+import { useRouter } from 'next/router';
 import { memo } from 'react';
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
@@ -11,10 +12,16 @@ interface SubscribeButtonProps {
 
 function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession();
+  const router = useRouter()
 
   async function handleSubscribe() {
     if(!session) {
       signIn('github');
+      return;
+    }
+
+    if(session.activeSubscription) {
+      router.push('/posts')
       return;
     }
 
